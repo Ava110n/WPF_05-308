a2773a9f44c929ef6a16ad09bb7bace7cb23a9ef
RuVMakarov <RuVMakarov@IMM-14-509-1M>
2025-03-27 15:39:18 +0300

    1#

--- WPF_05-308/MainWindow.xaml.cs
+++ WPF_05-308/MainWindow.xaml.cs
@@ -16,15 +16,53 @@ namespace WPF_05_308;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private double x = 0;
+    private double y = 0;
+    private string z = "";
+
     public MainWindow()
     {
         InitializeComponent();
     }
 
-    void onClick(object sender, RoutedEventArgs e)
+    void onClickNumber(object sender, RoutedEventArgs e)
     {
-        //MessageBox.Show(sender.ToString().Split(" ")[1]);
-        var text = (Button)sender;
-        Calc.Text += text.Content.ToString();
+        var btn = (Button)sender;
+        var number = (string)btn.Content;
+
+        Screen.Text += number;
+        y = y * 10 + Convert.ToInt32(number);
+    }
+    void onClickSign(object sender, RoutedEventArgs e)
+    {
+        var btn = (Button)sender;
+        var sign = (string)btn.Content;
+
+        if (Screen.Text[Screen.Text.Length-1]=='+' || Screen.Text[Screen.Text.Length - 1] == '-' ||
+            Screen.Text[Screen.Text.Length - 1] == '*' || Screen.Text[Screen.Text.Length - 1] == '/')
+        {
+            z = sign;
+            Screen.Text = Screen.Text.Remove(Screen.Text.Length - 1, 1);
+            Screen.Text += sign;
+            return;
+        }
+
+        if (z == "")
+        {
+            x = y;
+        }
+        else
+        {
+            switch (z)
+            {
+                case "+": x = x + y; break;
+                case "-": x = x - y; break;
+                case "*": x = x * y; break;
+                case "/": x = x / y; break;
+            }
+        }
+        z = sign;
+        y = 0;
+        Screen.Text = x + z;
     }
 }
