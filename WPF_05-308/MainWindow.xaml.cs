c33ce17c8a41d5c5f0e2423704e085dc036b8e1c
RuVMakarov <RuVMakarov@IMM-14-509-1M>
2025-03-27 13:58:10 +0300

    Добавьте файлы проекта.

---
+++ WPF_05-308/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_05_308;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    void onClick(object sender, RoutedEventArgs e)
+    {
+        //MessageBox.Show(sender.ToString().Split(" ")[1]);
+        var text = (Button)sender;
+        Calc.Text += text.Content.ToString();
+    }
+}
